5ac2f600aa573a3b559bb6085f9af23809298b5b
lpalacinji-lgtm <lpalacinji@gmail.com>
2025-11-23 18:35:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,26 @@ if st.session_state.fase == "formulario":
         # Si NO existe
         else:
             st.warning("Documento no encontrado.")
+if st.button("Registrar nuevo usuario"):
+                st.session_state.nuevo_documento = str(documento)
+                st.session_state.fase = "nuevo_registro"
+                st.rerun()
 
+
+# -------------------------------
+# FASE 2: NUEVO REGISTRO
+# -------------------------------
+elif st.session_state.fase == "nuevo_registro":
+    st.title("📝 Registrar nuevo usuario")
+
+    documento = st.session_state.get("nuevo_documento", "")
+    st.text_input("Documento", value=documento, disabled=True)
+
+    nombre = st.text_input("Nombre completo")
+    celular = st.text_input("Celular")            if st.button("Registrar nuevo usuario"):
+                st.session_state.nuevo_documento = str(documento)
+                st.session_state.fase = "nuevo_registro"
+                st.rerun()
             
 
     if st.button("Guardar nuevo usuario"):
@@ -342,6 +361,7 @@ elif st.session_state.fase == "confirmar":
         st.session_state.mesa = ""
         st.experimental_set_query_params()
         st.rerun()
+
 
 
 
