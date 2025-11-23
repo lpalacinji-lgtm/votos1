2ddebb5cd050b3129422a6f7fff5c1484ceb5c63
lpalacinji-lgtm <lpalacinji@gmail.com>
2025-11-23 18:36:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -98,23 +98,24 @@ if st.session_state.fase == "formulario":
         # Si NO existe
         else:
             st.warning("Documento no encontrado.")
-if st.button("Registrar nuevo usuario"):
+                
+            if st.button("Registrar nuevo usuario"):
                 st.session_state.nuevo_documento = str(documento)
                 st.session_state.fase = "nuevo_registro"
                 st.rerun()
 
 
-# -------------------------------
-# FASE 2: NUEVO REGISTRO
-# -------------------------------
-elif st.session_state.fase == "nuevo_registro":
-    st.title("📝 Registrar nuevo usuario")
+            # -------------------------------
+            # FASE 2: NUEVO REGISTRO
+            # -------------------------------
+            elif st.session_state.fase == "nuevo_registro":
+            st.title("📝 Registrar nuevo usuario")
 
-    documento = st.session_state.get("nuevo_documento", "")
-    st.text_input("Documento", value=documento, disabled=True)
+            documento = st.session_state.get("nuevo_documento", "")
+            st.text_input("Documento", value=documento, disabled=True)
 
-    nombre = st.text_input("Nombre completo")
-    celular = st.text_input("Celular")            if st.button("Registrar nuevo usuario"):
+            nombre = st.text_input("Nombre completo")
+            celular = st.text_input("Celular")            if st.button("Registrar nuevo usuario"):
                 st.session_state.nuevo_documento = str(documento)
                 st.session_state.fase = "nuevo_registro"
                 st.rerun()
@@ -361,6 +362,7 @@ elif st.session_state.fase == "confirmar":
         st.session_state.mesa = ""
         st.experimental_set_query_params()
         st.rerun()
+
 
 
 
